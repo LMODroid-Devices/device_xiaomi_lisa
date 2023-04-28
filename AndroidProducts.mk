ee5a92e11279754b914785fa69ab71b99b1a1ce9
LMO GerritBot <gerrit@libremobileos.com>
2023-04-28 04:12:09 +0000

    Adapt to LMODroid by device-add script
    Change-Id: Ied5e76a5fd0cbc8a0b66fca03fe7d2b2d8adeacd

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lisa.mk
+    $(LOCAL_DIR)/lmodroid_lisa.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lisa-user \
-    lineage_lisa-userdebug \
-    lineage_lisa-eng
+    lmodroid_lisa-user \
+    lmodroid_lisa-userdebug \
+    lmodroid_lisa-eng
