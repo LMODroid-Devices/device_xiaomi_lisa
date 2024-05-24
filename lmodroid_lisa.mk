d96a5589ef2cbfcc34448b92e1a15c8f2d2b8255
LMO GerritBot <gerrit@libremobileos.com>
2024-05-24 04:09:50 +0000

    Adapt to LMODroid by device-add script

--- lmodroid_lisa.mk
+++ lmodroid_lisa.mk
@@ -12,13 +12,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lisa
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2109119DG
-PRODUCT_NAME := lineage_lisa
+PRODUCT_NAME := lmodroid_lisa
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
